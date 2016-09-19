874d1536295fc9fcfd08a30579d563a672ff4df9
kevin <kevin@LAPTOP-9I3SBEFT>
2016-09-19 12:16:46 +0200

    Suite

---
+++ src/main/java/Zoo/zoo.java
@@ -0,0 +1,16 @@
+package Zoo;
+
+import java.util.ArrayList;
+
+import animal.animal;
+
+public class zoo {
+
+	ArrayList<animal> pensionnaires = new ArrayList<animal>();
+	
+	public ArrayList<animal> getPensionnaires()
+	{
+		return pensionnaires;
+	}
+	
+}
